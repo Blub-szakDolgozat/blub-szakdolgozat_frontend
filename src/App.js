889f552782665c8c641fbaac33470e2ea4c5c7cf
DDorka02 <dobszaydorka@gmail.com>
2025-01-09 18:36:22 +0100

    profil kép behelyezése, és css szépítése

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
           <h1>Blub</h1>
+          <div>
+        <img
+          src="/kepek/Fiokikon.png"
+          alt="ikon"
+        />
+          </div>
+
       </header>
     </div>
+
   );
 }
 
