3dd32f0abc7f4e5a5ceb33ccbd681a976e310af1
MidnightCrow00 <orsolyaalbert1@gmail.com>
2025-01-10 15:09:50 +0100

    Layout szerkesztése

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Nav } from "react-bootstrap";
+import '../App.css';
 
-export default function Layout() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Layout = () => {
+    return (
+        <>
+            <Nav>
+                <Nav.Item> {/* Középre igazítás támogatása */}
+                    <Nav.Link
+                        href="/admin"
+                        className="nav-link-black"
+                        style={{
+                            color: "white",
+                        }}
+                    >
+                        Admin
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link
+                        href="/"
+                        className="nav-link-black"
+                        style={{
+                            color: "white",
+                        }}
+                    >
+                        Public
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
+
+        </>
+    );
+};
+
+export default Layout;
--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NoPage() {
   return (
     <div>
-      
+        Kért oldal  nem található
     </div>
   )
 }
