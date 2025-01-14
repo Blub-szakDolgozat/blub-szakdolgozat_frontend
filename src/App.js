6c4138b52cb0a9fb269049224f4ba421fe680294
DDorka02 <dobszaydorka@gmail.com>
2025-01-14 18:57:16 +0100

    halak hozzáadása

--- src/App.js
+++ src/App.js
@@ -8,13 +8,9 @@ import { Routes, Route } from 'react-router-dom';
 import AkvariumPublic from './components/public/AkvariumPublic';
 import ProfileKep from './components/ProfilKep';
 import NapiSorsolas from './components/public/NapiSorsolas';
-
-
 import Profil from './pages/Profil';
 
 function App() {
-  
-
   const handleProfileClick = () => {
     alert('Profilképre kattintottál!');
   };
@@ -27,18 +23,28 @@ function App() {
           <ProfileKep onClick={handleProfileClick} />
         </div>
       </header>
-        <Routes>
-          {/* Layout és Public komponensek */}
-          <Route path="/" element={<Layout />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route path="/*" element={<Public />} />
-            <Route path="admin" element={<Admin />} />
-            <Route path="*" element={<NoPage />} />
-            <Route path="/akvarium" element={<AkvariumPublic />} />
-            <Route path="/sorsolas" element={<NapiSorsolas />} />
-            <Route path="/profil" element={<Profil />} />
-          </Route>
-        </Routes>
-        
+          <Route path="admin" element={<Admin />} />
+          <Route path="*" element={<NoPage />} />
+          <Route path="/akvarium" element={<AkvariumPublic />} />
+          <Route path="/sorsolas" element={<NapiSorsolas />} />
+          <Route path="/profil" element={<Profil />} />
+        </Route>
+      </Routes>
+      <div className="fish">
+        <div className="head">
+          <div className="eye"></div>
+        </div>
+        <div className="tail"></div>
+      </div> {/* Alap hal */}
+      <div className="fish fish2">
+        <div className="head">
+          <div className="eye"></div>
+        </div>
+        <div className="tail"></div>
+      </div> {/* Másik hal */}
     </div>
   );
 }
