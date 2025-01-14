26bda9eb0b4d35c9fc8c62a44313693dcdcb7ac3
midnightcrow00 <orsolyaalbert1@gmail.com>
2025-01-14 12:24:25 +0100

    javitas

--- src/pages/Bejelentkezes.js
+++ src/pages/Bejelentkezes.js
@@ -1,30 +1,27 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
-import { myAxios } from "../contexts/MyAxios";
+import { AuthContext } from "../contexts/AuthContext";
+
 
 export default function Bejelentkezes({ onLogin }) {
   const [email, setEmail] = useState("");
-  const [regi_jelszo, setPassword] = useState("");
-  const [error, setError] = useState(null); // Hibakezeléshez
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
+  const {login} =useContext(AuthContext);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
 
-    try {
-      const { data } = await myAxios.post("/login", { email, regi_jelszo });
-      console.log("Bejelentkezés sikeres:", data);
-
-      localStorage.setItem("authToken", data.token); 
-      onLogin(data.user); 
-
-      navigate("/home");
-    } catch (error) {
-      console.error("Bejelentkezési hiba:", error.response?.data || error.message);
-      setError(error.response?.data?.message || "Hiba történt a bejelentkezés során.");
-    }
-  };
+  function handleSubmit(event) {
+    event.preventDefault();
+    const adat = {
+        email: email,
+        password: password,
+    };
+    console.log("Bejelentkezés sikeres:", adat);
+    login(adat);
+    navigate("/home");
+  }
 
   const handleClick = () => {
     navigate("/regisztralas");
@@ -47,12 +44,12 @@ export default function Bejelentkezes({ onLogin }) {
                 required
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="regi_jelszo">
+            <Form.Group className="mb-3" controlId="password">
               <Form.Label>Jelszó</Form.Label>
               <Form.Control
                 type="password"
                 placeholder="Add meg a jelszavad"
-                value={regi_jelszo}
+                value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
