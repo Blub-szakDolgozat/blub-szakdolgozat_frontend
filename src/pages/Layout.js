ccbd50fd75d286ad371c499f658abf88947fd030
midnightcrow00 <orsolyaalbert1@gmail.com>
2025-01-14 14:26:38 +0100

    hamburger ikon

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import { Nav } from 'react-bootstrap'; 
-import { Link, Outlet } from 'react-router-dom';  
-import './Layout.css'; 
+import React, { useState, useEffect } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import './Layout.css';
 
 const Layout = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = (e) => {
+    // Csak akkor zárja be a menüt, ha a kattintás nem a menü vagy a hamburger ikon területén történt
+    if (!e.target.closest('.dropdown-menu') && !e.target.closest('.hamburger')) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', closeMenu);
+    return () => {
+      document.removeEventListener('click', closeMenu);
+    };
+  }, []);
+
   return (
     <div>
-      <Nav className="navbar custom-navbar">
-        <Link to="/admin" className="nav-link-black">
+      {/* Hamburger ikon */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+
+      {/* Menü (legördülős) */}
+      <nav className={`dropdown-menu ${isMenuOpen ? 'open' : ''}`}>
+        <Link to="/admin" className="nav-link">
           Admin
         </Link>
-        <Link to="/bejelentkezes" className="nav-link-black">
+        <Link to="/bejelentkezes" className="nav-link">
           Bejelentkezés
         </Link>
-        <Link to="/akvarium" className="nav-link-black">
+        <Link to="/akvarium" className="nav-link">
           Akvárium
         </Link>
-      </Nav>
+      </nav>
 
+      {/* Tartalom */}
       <div className="content">
         <Outlet />
       </div>
