837a6ba60471ce8f521c7699912d2767c463f344
midnightcrow00 <orsolyaalbert1@gmail.com>
2025-01-14 13:22:01 +0100

    CleanCode

--- src/pages/Public.js
+++ src/pages/Public.js
@@ -1,25 +1,16 @@
-import React, { useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import React from "react";
+import { Routes, Route} from "react-router-dom";
 import Bejelentkezes from "./Bejelentkezes";
 import Regisztralas from "./Regisztralas";
 
 export default function Public() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const navigate = useNavigate();
-
-  // Bejelentkezési állapot frissítése
-  const handleLogin = () => {
-    setIsLoggedIn(true);  // Frissíti az isLoggedIn állapotot
-    navigate("/");        // Navigálás a főoldalra
-  };
 
   return (
     <div>
         <Routes>
           {/* Bejelentkezés és regisztráció */}
-          <Route path="/bejelentkezes" element={<Bejelentkezes onLogin={handleLogin} />} />
+          <Route path="/bejelentkezes" element={<Bejelentkezes />} />
           <Route path="/regisztralas" element={<Regisztralas />} />
-          <Route path="*" element={<Bejelentkezes onLogin={handleLogin} />} />
         </Routes>
     </div>
   );
