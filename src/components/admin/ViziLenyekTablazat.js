bd6b48e8929ec16a3c65f7bec074ee8ccb8cf832
midnightcrow00 <orsolyaalbert1@gmail.com>
2025-01-21 11:34:12 +0100

    vizilények tábla

--- src/components/admin/ViziLenyekTablazat.js
+++ src/components/admin/ViziLenyekTablazat.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Table, Col } from "react-bootstrap";
+import ViziLenyekTabla from "./ViziLenyekTabla";
 
-export default function ViziLenyekTablazat() {
+export default function Dogak(props) {
   return (
-    <div>
-      
-    </div>
-  )
+    <Row>
+      <Container className="mt-4">
+          <Table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Képe</th>
+                <th>Vízilény Neve</th>
+                <th>Vízilény Fajtája</th>
+                <th>Ritkasági szinte</th>
+                <th>Leírása</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.kepekLista.map((element) => {
+                return <ViziLenyekTabla adat={element} key={element.id} />;
+              })}
+            </tbody>
+          </Table>
+      </Container>
+    </Row>
+  );
 }
