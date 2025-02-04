319f25e2ea8d1fb0e30c15a05195e4381098807f
midnightcrow00 <orsolyaalbert1@gmail.com>
2025-02-04 09:18:25 +0100

    javitasok

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -6,9 +6,10 @@ export const AuthContext = createContext("");
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [userProfilePic, setUserProfilePic] = useState(null)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userProfilePic, setUserProfilePic] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
+  const [token, setToken] = useState(localStorage.getItem("access_token"));
 
   // CSRF cookie megszerzése
   const csrf = async () => {
@@ -34,14 +35,18 @@ export const AuthProvider = ({ children }) => {
   const login = async ({ email, password }) => {
     await csrf();
     try {
-      const { data } = await myAxios.post("/login", { email, password });
-      setUser(data.user);
-      setIsLoggedIn(true);
+      const response = await myAxios.post("/login", {
+        email: email,
+        password: password,
+      });
+      setUser(response.data.user);
+      setToken(response.data.access_token);
+      localStorage.setItem("access_token", response.data.token);
+      getUser();
       const storedProfilePic = localStorage.getItem("userProfilePic");
       setUserProfilePic(
         storedProfilePic || "https://www.w3schools.com/howto/img_avatar.png"
       );
-      localStorage.setItem("access_token", data.access_token);
       navigate("/akvarium");
     } catch (error) {
       console.log(
@@ -53,18 +58,26 @@ export const AuthProvider = ({ children }) => {
 
   // Felhasználó adatainak lekérése
   const getUser = async () => {
-    try {
-      const { data } = await myAxios.get("/user");
-      setUser(data);
-      const storedProfilePic = localStorage.getItem("userProfilePic");
-      setUserProfilePic(
-        storedProfilePic || "https://www.w3schools.com/howto/img_avatar.png"
-      );
-    } catch (error) {
-      console.log(
-        "Felhasználó lekérdezési hiba:",
-        error.response ? error.response.data : error.message
-      );
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      try {
+        const response = await myAxios.get("/user", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        console.log("Felhasználói adat:", response.data);
+        setUser(response.data);
+        const storedProfilePic = localStorage.getItem("userProfilePic");
+        setUserProfilePic(
+          storedProfilePic || "https://www.w3schools.com/howto/img_avatar.png"
+        );
+      } catch (error) {
+        console.log(
+          "Felhasználó lekérdezési hiba:",
+          error.response ? error.response.data : error.message
+        );
+      }
     }
   };
 
@@ -73,7 +86,7 @@ export const AuthProvider = ({ children }) => {
     await csrf();
     try {
       await myAxios.post("/logout");
-      setUser("");
+      setUser(null);
       setIsLoggedIn(false);
       setUserProfilePic(null); // Profilkép alaphelyzetbe állítása
       localStorage.removeItem("userProfilePic");
@@ -84,57 +97,57 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-    // Profilkép frissítése
-    const updateProfilePic = (newProfilePic) => {
-      setUserProfilePic(newProfilePic);
-      localStorage.setItem("userProfilePic", newProfilePic);
-    };
-  
-    // Fájl feltöltése és base64-re konvertálás
-    const handleFileUpload = (event) => {
-      const file = event.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onloadend = () => {
-          const base64Image = reader.result; // Base64 kép
-          updateProfilePic(base64Image); // Frissítés
-        };
-        reader.readAsDataURL(file);
-      }
-    };
-  
-    // Profilkép betöltése az alkalmazás indításakor
-    useEffect(() => {
-      const storedProfilePic = localStorage.getItem("userProfilePic");
-      if (storedProfilePic) {
-        setUserProfilePic(storedProfilePic);
-      } else {
-        setUserProfilePic("https://www.w3schools.com/howto/img_avatar.png"); // Alapértelmezett profilkép
-      }
-    }, []);
+  // Profilkép frissítése
+  const updateProfilePic = (newProfilePic) => {
+    setUserProfilePic(newProfilePic);
+    localStorage.setItem("userProfilePic", newProfilePic);
+  };
 
-    useEffect(() => {
-      const fetchUser = async () => {
-        try {
-          const response = await fetch('/user'); // A backend automatikusan ellenőrzi a CSRF cookie-t
-          if (!response.ok) {
-            throw new Error('Felhasználói adatok lekérése nem sikerült');
-          }
-          const data = await response.json();
-          setUser(data); // A válasz tartalma (felhasználói adatok)
-        } catch (error) {
-          console.error('Hiba:', error);
-        }
+  // Fájl feltöltése és base64-re konvertálás
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64Image = reader.result; // Base64 kép
+        updateProfilePic(base64Image); // Frissítés
       };
-    
-      fetchUser();
-    }, []);
-    
-    
+      reader.readAsDataURL(file);
+    }
+  };
+
+  // Profilkép betöltése az alkalmazás indításakor
+  useEffect(() => {
+    getUser();
+    const storedProfilePic = localStorage.getItem("userProfilePic");
+    if (storedProfilePic) {
+      setUserProfilePic(storedProfilePic);
+    } else {
+      setUserProfilePic("https://www.w3schools.com/howto/img_avatar.png"); // Alapértelmezett profilkép
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("/user"); // A backend automatikusan ellenőrzi a CSRF cookie-t
+        if (!response.ok) {
+          throw new Error("Felhasználói adatok lekérése nem sikerült");
+        }
+        const data = await response.json();
+        setUser(data); // A válasz tartalma (felhasználói adatok)
+      } catch (error) {
+        console.error("Hiba:", error);
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   return (
     <AuthContext.Provider
       value={{
+        getUser,
         isLoggedIn,
         regisztracio,
         login,
@@ -142,7 +155,7 @@ export const AuthProvider = ({ children }) => {
         logout,
         userProfilePic,
         updateProfilePic,
-        handleFileUpload
+        handleFileUpload,
       }}
     >
       {children}
