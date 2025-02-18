63d2ac036a7ff12394b1a86f32818972a9263538
horvathnauzika <naihorvath@gmail.com>
2025-02-18 11:00:20 +0100

    admin felületen a nav linkek betűszíne megváltozik, ha rákattintunk

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,53 +1,42 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { Container, Nav } from "react-bootstrap";
+import './Admin.css';
 
 export default function Admin() {
   return (
     <Container>
       <Nav style={{ display: "flex", gap: "0" }}>
         <Nav.Item>
-          <Link
+          <NavLink
             to="/admin/vizilenyekurlap"
             className="nav-link"
-            style={{ fontWeight: "bold", textDecoration: "none" }} // Félkövér és link aláhúzás eltávolítása
-            onMouseOver={(e) => (e.target.style.textDecoration = "underline")} // Hover: aláhúzás
-            onMouseOut={(e) => (e.target.style.textDecoration = "none")} // Hover elhagyáskor: aláhúzás eltávolítása
           >
             Vízilények
-          </Link>
+          </NavLink>
         </Nav.Item>
         <Nav.Item>
-          <Link
+          <NavLink
             to="/admin/cikkekurlap"
             className="nav-link"
-            style={{ fontWeight: "bold", textDecoration: "none" }} 
-            onMouseOver={(e) => (e.target.style.textDecoration = "underline")} 
-            onMouseOut={(e) => (e.target.style.textDecoration = "none")} 
           >
             Cikkek
-          </Link>
+          </NavLink>
         </Nav.Item>
         <Nav.Item>
-          <Link
+          <NavLink
             to="/admin/esemenyekurlap"
             className="nav-link"
-            style={{ fontWeight: "bold", textDecoration: "none" }} // Félkövér és link aláhúzás eltávolítása
-            onMouseOver={(e) => (e.target.style.textDecoration = "underline")} // Hover: aláhúzás
-            onMouseOut={(e) => (e.target.style.textDecoration = "none")} // Hover elhagyáskor: aláhúzás eltávolítása
           >
             Események
-          </Link>
+          </NavLink>
         </Nav.Item>
         <Nav.Item>
-          <Link
+          <NavLink
             to="/admin/videokurlap"
             className="nav-link"
-            style={{ fontWeight: "bold", textDecoration: "none" }} // Félkövér és link aláhúzás eltávolítása
-            onMouseOver={(e) => (e.target.style.textDecoration = "underline")} // Hover: aláhúzás
-            onMouseOut={(e) => (e.target.style.textDecoration = "none")} // Hover elhagyáskor: aláhúzás eltávolítása
           >
             Videók
-          </Link>
+          </NavLink>
         </Nav.Item>
       </Nav>
       <div className="content">
